dbe9648b1cd20f6f59f7a1f5b5bc1a12600217c8
ADM2331 <146051140+ADM2331@users.noreply.github.com>
2024-04-01 12:03:31 +0800

    Add files via upload

---
+++ STRUKTUR DATA/meet2.py
@@ -0,0 +1,20 @@
+# belajar variable
+#assigmant
+x = 100
+y = 6
+uang = 1000
+
+#pemanggiilan
+print ("Nilai Uas saya =", x)
+print ("Nilai Uas saya =", y)
+print ("Nilai Uas saya =", uang)
+
+#penamaan variable
+nilai_a = 9
+kotak10 = 10
+
+#input variable kedua
+x = 30
+print ("Nilai Uas saya =", x)
+x = 3000
+print ("Nilai Uas saya =", x)    #tidak ada batasan untuk mendoubeuble variable (unlimited)
