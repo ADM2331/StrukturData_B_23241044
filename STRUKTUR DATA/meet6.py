28bf5b5a06c87e61588924440d7a80f4114df803
ADM2331 <146051140+ADM2331@users.noreply.github.com>
2024-05-06 11:46:20 +0800

    Add files via upload

---
+++ STRUKTUR DATA/meet6.py
@@ -0,0 +1,50 @@
+                                                            #PERTETMUAN HARI SENIN 6 MEI 2024
+
+# Materi ke 6                                                          (PERBANDINGAN)
+
+# MACAM MACAM PERBANDINGAN
+#1. lebih besar >
+#2. lebih kecil <
+#3. lebih besar sama dengan >= 
+#4. lebih kecil sama dengan <=
+#5. tidak sama dengan!=
+#6. sama "is" ==
+#7. tidak sama "is not" !=
+ 
+
+x = 4
+y = 5
+
+#1. lebih besar >
+hasil = x > y
+print(x,'>',  y, 'adalah', hasil)
+
+#2. lebih kecil <
+hasil = x < y
+print(x,'<',  y, 'adalah', hasil)
+
+#3. lebih besr sama dengan >= 
+hasil = x >= y
+print(x,'>=', y, 'adalah', hasil)
+
+#4. lebih kecil sama dengan <=
+hasil = x <= y
+print(x,'<=', y, 'adalah', hasil)
+
+#5. tidak sama dengan!=
+hasil = x != y
+print(x,'!=', y, 'adalah', hasil)
+
+#6. sama dengan "is" ==
+hasil = x is y
+print(x,'is', y, 'adalah', hasil)
+
+#7. tidak sama "is not" !=
+hasil = x is not y
+print(x,'is not', y, 'adalah', hasil)
+
+# > < >= <= == != ini adalah perbandingan literal
+# x = 4, 4 = perbandingan literal 
+# x is 4 (tidak bisa, karena yang di bandingkan adalah literal)
+# x is y (bisa, karena yang di bandingkan adalah objact)
+
