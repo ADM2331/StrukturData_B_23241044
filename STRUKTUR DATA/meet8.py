425f7dd2fb310c7618ab35b761eb09757eadeede
ADM2331 <146051140+ADM2331@users.noreply.github.com>
2024-05-20 12:36:58 +0800

    Add files via upload
    SENIN,20 MEI 2024 (PERBANDINGAN LANJUTAN)

---
+++ STRUKTUR DATA/meet8.py
@@ -0,0 +1,50 @@
+                                    #senin-20 Mei 2024
+                                  #PERBANDINGAN LANJUTAN
+# +++3 --- 9+++
+Masukkanuser = float (input("masukkan bilangan \n kurang dari 3 atau \n lebih dari 9 :"))
+print (Masukkanuser)
+
+#--------CEK KURANG ARI 3
+kurdar =(Masukkanuser <3)
+print ("kurang dari 3 = ", kurdar )
+
+#--------CEK lLEBIH DARI 9
+ledar = (Masukkanuser >9)
+print ("ebih dari 9 = ", ledar)
+
+betul = kurdar or ledar
+print ("pngecekan Final :", betul)
+
+
+
+# 4 14
+#cara saya yang menggunakan or
+Masukkanuser = float(input("masukkan bilangan antara 4 dan 14 :" ))
+
+lebih =(Masukkanuser >4)
+Kurang =(Masukkanuser <14)
+
+Benar = lebih or Kurang
+print("bilangan antara 4 dan 14 adalah :", Benar)
+
+
+#cara bapaknya yang menggunakan and
+Masukkanuser = float(input("masukkan bilangan antara 4 dan 14 :" ))
+print (Masukkanuser)
+
+#--------CEK KURANG ARI 14
+kurdar =(Masukkanuser <14)
+print ("kurang dari 3 = ", kurdar )
+
+#--------CEK lLEBIH DARI 4
+ledar = (Masukkanuser >4)
+print ("ebih dari 9 = ", ledar)
+
+betul = kurdar and ledar
+print ("pngecekan Final :", betul)
+
+
+
+
+# ----5++++9-----12++++30---- (sebagai PR)
+
