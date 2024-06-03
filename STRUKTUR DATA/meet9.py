2d30299c891c541d3555ce2c22bee505c1df79cc
ADM2331 <146051140+ADM2331@users.noreply.github.com>
2024-06-03 13:28:41 +0800

    Add files via upload
    Senin 03 Juni 2024

---
+++ STRUKTUR DATA/meet9.py
@@ -0,0 +1,29 @@
+                                                   #Senin 3 Juni 2023
+                                                 #PERCABANGAN JIKA MAKA
+
+#contoh pemerograman linier (materi sebelumnya)
+name = input ("what is iyour name :")
+print (name)
+
+#percabangan
+#if kondisi : aksi
+
+  #1. if standar
+#if name == "adam" : print("Mahasiswa")
+#print("Program Selesai") 
+
+  #2. if indentasi
+#if name == "adam" :
+#    print ("Mahasiswa")
+#    print ("Mahasiswa PTI")
+#print("Program selesai")
+
+
+  #3. if else
+if name == "adam" :
+    print("Mahasiswa PTI")
+    print(f"Terimakasih {name}")
+else:
+    print("Bukan Mahasiswa PTI")
+print ("PROGRAM SELESAI")
+
