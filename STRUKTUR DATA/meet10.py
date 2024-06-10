e0d8b994118a0737b09ed38aea517ffdfe4f69fb
ADM2331 <146051140+ADM2331@users.noreply.github.com>
2024-06-10 13:31:25 +0800

    Add files via upload
    Pertemuan Senin 10 juni 2024

---
+++ STRUKTUR DATA/meet10.py
@@ -0,0 +1,53 @@
+                                            #PERTEMUAN SENIN 10 JUNI 2024
+
+#LANJUTAN DARI PERCABANGAN PERTEMUAN KE 9
+
+#Membuat percabangan yang lebih dari 1
+nama = input("masukkan nama :")
+
+if nama   =="Adam":
+    Print ("Mahasiswa PTI B")
+elif nama =="Chelly":
+    print ("Mahasiswa PTI B")
+elif nama == "Ridho":
+    print ("Mahasiswa PTI B")
+elif nama == "Bima":
+    print("Mahasiswa PTI B")
+elif nama == "Budi":
+    print ("Mahasiswa PTI B")
+elif nama == "udin":
+    print("Mahasiswa Kehutanan")
+elif nama =="Adin":
+    print("Mahasiswa PTI B")
+
+else:
+    print("Bukan Mahasiswa PTI B")
+print("Program selesai")
+
+
+
+                        #PEMBUATAN PENJUMLAHAN DAN PENGURANGAN  PERTAMA
+#PenjumlahanPengurangan = input("Silahkan Masukkan Bilangan 1 atau 2 Untuk Masuk Preogram :")
+
+#if PenjumlahanPengurangan == "1":
+    
+#    print("Anda Masuk Di Perogram Penjumlahan")
+
+
+#elif PenjumlahanPengurangan =="2":
+
+#    print("Anda Masuk Di Program Pengurangan")
+
+#else :
+#    print("Bukan Program Penjumlahan dan Pengurangan")
+
+
+
+
+
+
+
+
+
+    
+    
